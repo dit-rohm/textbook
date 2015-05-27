481b15da0cbda7f6441891c06aca4e2e6690f896
Shun Tamura <tamrin.bitterchococo@gmail.com>
2015-05-27 19:32:41 +0900

    残タスクを消化

--- 7/script.js
+++ 7/script.js
@@ -1,47 +1,93 @@
+var type = {
+  king: 0, officer: 0, scholar: 0, craftsman: 0
+}
+
 function result () {
-  // 各回答をanswer配列に格納
+  var answer = checkAnswer();
+
+  countType(answer);
+
+  var max = getMax(answer);
+
+  var result = getResult(max)
+
+  showResult(result);
+}
+
+function checkAnswer() {
+  // 各回答をreturnAnswer配列に格納
   var total = document.shindan.length;
-  var answer = [];
+  var returnAnswer = [];
   var j = 0;
   for (var i = 0; i < total; i++) {
     if (document.shindan.elements[i].checked) {
-      answer.push(document.shindan.elements[i].value);
+      returnAnswer.push(document.shindan.elements[i].value);
     }
   }
 
+  return returnAnswer;
+}
+
+function countType(answer) {
   // それぞれ数える
-  var cnt = [ ['king', 0], ['officer', 0], ['scholar', 0], ['craftsman', 0] ];
   for (var i = 0; i < answer.length; i++) {
     switch(answer[i]){
       case 'king':
-        cnt[0][1]++;
+        type.king++;
         break;
       case 'officer':
-        cnt[1][1]++;
+        type.officer++;
         break;
       case 'scholar':
-        cnt[2][1]++;
+        type.scholar++;
         break;
       case 'craftsman':
-        cnt[3][1]++;
+        type.craftsman++;
         break;
       default:
         alert('error!!');
         break;
     }
   }
+}
 
-  // cnt配列を降順にソート
-  cnt.sort(function(a, b){return(b[1] - a[1]);});
+function getMax(answer) {
+  /*
+   * 回答数が多いプロパティのキーを取りたい
+   * 最大回答数の5から降ろしていく
+   */
+  for (var i = answer.length; i > 0; i--) {
+    var returnMax = getKey(i);
+    if (returnMax.length > 0) break;
+  }
+  alert(returnMax);
+  return returnMax;
+}
 
-  // タイプ判定 カウント数が同じものがある場合はランダムに
-  var type = cnt[0][0];
-  if(cnt[0][1] == cnt[1][1]) {
-    type = cnt[Math.floor(Math.random() * 2)][0];
+// 一致するバリューがあればキーを配列で返す
+function　getKey(value) {
+    var returnKey = [];
+    for (var key in type) {
+        if (type[key] == value) {
+            returnKey.push(key);
+        }
+    }
+    return returnKey;
+}
+
+function getResult(max) {
+  if (max.length == 1) {
+    var returnResult = max[0];
+  } else {
+    returnResult = max[Math.floor(Math.random() * max.length)];
   }
 
+  return returnResult;
+}
+
+function showResult(result) {
   // タイプごとにページを移動させる
-  switch(type){
+  switch(result){
     case 'king':
       location.href = './king.html';
       break;
