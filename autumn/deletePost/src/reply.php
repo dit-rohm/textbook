c5199700279d74e70b21c1a56e04124e0d40b5ac
tomoki <tomofuma@gmail.com>
2015-11-04 19:05:43 +0900

    add deletePost script

---
+++ autumn/deletePost/src/reply.php
@@ -0,0 +1,20 @@
+<?php
+// PHP部分はここに書きます
+  
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>reply - Ditter</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+
+<!-- HTML部分はここに書きます -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
